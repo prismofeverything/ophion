3a4cb85402f6f7ae4bfdf092b9299c8e44f5d742
Ryan Spangler <ryan.spangler@gmail.com>
2020-01-13 23:57:18 -0800

    version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ophion "0.0.14"
+(defproject ophion "0.1.0"
   :description "graph queries as data"
-  :url "http://github.com/bmeg/ophion"
+  :url "http://github.com/prismofeverything/ophion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -12,9 +12,5 @@
                  [clojurewerkz/elastisch "2.2.1" :exclusions [commons-codec]]
                  [org.apache.tinkerpop/gremlin-core "3.4.1" :extension "pom"]
                  [com.novemberain/monger "3.1.0"]]
-  :repositories [["sonatype snapshots"
-                  "https://oss.sonatype.org/content/repositories/snapshots"]
-                 ["sonatype releases"
-                  "https://oss.sonatype.org/content/repositories/releases"]]
   :jvm-opts ["-Xmx4g" "-Xms4g"]
   :main ophion.server)
